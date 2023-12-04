7770e9f5922179143d5c1078919375fe8939b093
Roeliefantje <roel.de.jeu@gmail.com>
2023-12-04 16:09:10 +0100

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use bevy::{prelude::*, math::*};
-use rand::{thread_rng, Rng};
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+use bevy::{math::*, prelude::*};
+use rand::{thread_rng, Rng};
 
 pub const HEIGHT: f32 = 720.0;
 pub const WIDTH: f32 = 1080.0;
@@ -20,7 +20,7 @@ fn main() {
 
 #[derive(Component)]
 struct Crow {
-    vel: Vec3
+    vel: Vec3,
 }
 
 impl Default for Crow {
@@ -31,7 +31,7 @@ impl Default for Crow {
         let y_coords = rng.gen_range(0..200) as f32 / 100.0;
         let z_coords = rng.gen_range(0..200) as f32 / 100.0;
         Self {
-            vel: Vec3::new(x_coords, y_coords, z_coords).normalize()
+            vel: Vec3::new(x_coords, y_coords, z_coords).normalize(),
         }
     }
 }
@@ -39,26 +39,29 @@ impl Default for Crow {
 #[derive(Bundle)]
 struct CrowBundle {
     pbr: PbrBundle,
-    crow: Crow
+    crow: Crow,
 }
 
 impl Default for CrowBundle {
     fn default() -> Self {
         Self {
             pbr: PbrBundle::default(),
-            crow: Crow::default()
+            crow: Crow::default(),
         }
     }
 }
 
-
-fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
     // Camera
     commands.spawn(Camera3dBundle {
         transform: Transform::from_xyz(-20., 5., 0.).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
-    
+
     //paddle
     let size: usize = 1000;
     let mut all_cubes: Vec<CrowBundle> = Vec::with_capacity(size);
@@ -70,7 +73,7 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials
         let z_coords = rng.gen_range(-10000..10000) as f32 / 1000.0;
         let cube = CrowBundle {
             pbr: PbrBundle {
-                mesh: meshes.add(Mesh::from(shape::Cube {size: 0.1})),
+                mesh: meshes.add(Mesh::from(shape::Cube { size: 0.1 })),
                 material: materials.add(Color::rgb_u8(124, 144, 166).into()),
                 transform: Transform::from_xyz(x_coords, y_coords, z_coords),
                 ..default()
@@ -83,11 +86,11 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials
 }
 
 fn rotate(mut query: Query<&mut Transform, With<Crow>>, time: Res<Time>) {
-
     for mut transform in &mut query {
         transform.rotate_y(time.delta_seconds() / 2.)
     }
 }
+
 fn borders(mut query: Query<&mut Transform, With<Crow>>) {
     for mut transform in query.iter_mut() {
         if transform.translation.x < -10. {
@@ -96,7 +99,7 @@ fn borders(mut query: Query<&mut Transform, With<Crow>>) {
         if transform.translation.x > 10. {
             transform.translation.x = -10.;
         }
-        
+
         if transform.translation.y < -10. {
             transform.translation.y = 10.;
         }
@@ -109,22 +112,23 @@ fn borders(mut query: Query<&mut Transform, With<Crow>>) {
         if transform.translation.z > 10. {
             transform.translation.z = -10.;
         }
-    } 
+    }
 }
 
-fn apply_velocity(mut query: Query<(&mut Transform, &Crow)>, time: Res<Time>){
+fn apply_velocity(mut query: Query<(&mut Transform, &Crow)>, time: Res<Time>) {
     for (mut transform, crow) in query.iter_mut() {
         //transform.translation += crow.vel * time.delta_seconds();
         let new_pos = transform.translation + crow.vel * time.delta_seconds();
         transform.look_at(new_pos, Vec3::Y);
-        transform.translation = new_pos; 
+        transform.translation = new_pos;
         //println!("{}", crow.vel);
     }
 }
 
-fn crow_behaviour(mut query: Query<(&Transform, &mut Crow)>, others: Query<&Transform, With<Crow>>)
-{
-
+fn crow_behaviour(
+    mut query: Query<(&Transform, &mut Crow)>,
+    others: Query<&Transform, With<Crow>>,
+) {
     for (transform, mut crow) in query.iter_mut() {
         let seperation = calculate_seperation(&transform, &others);
         let alignment = calculate_alignment(&transform, &others);
@@ -137,15 +141,15 @@ fn crow_behaviour(mut query: Query<(&Transform, &mut Crow)>, others: Query<&Tran
         crow.vel += seperation + alignment + cohesion;
 
         crow.vel = crow.vel.normalize() * 2.0;
-    } 
+    }
 }
 
 const SEPERATION_RADIUS: f32 = 1.2;
 const VISION_RADIUS: f32 = 3.0;
 
-fn calculate_seperation(boid: &Transform, others: &Query<&Transform, With<Crow>>) -> Vec3{
+fn calculate_seperation(boid: &Transform, others: &Query<&Transform, With<Crow>>) -> Vec3 {
     let mut total_seperation: Vec3 = Vec3::ZERO;
-    
+
     for other_crows in others.iter() {
         let diff: f32 = boid.translation.distance(other_crows.translation);
 
@@ -159,12 +163,11 @@ fn calculate_seperation(boid: &Transform, others: &Query<&Transform, With<Crow>>
     total_seperation
 }
 
-fn calculate_alignment(boid: &Transform, others: &Query<&Transform, With<Crow>>) -> Vec3{
+fn calculate_alignment(boid: &Transform, others: &Query<&Transform, With<Crow>>) -> Vec3 {
     let mut total_alignment: Vec3 = Vec3::ZERO;
 
     for other_crows in others.iter() {
         let diff: f32 = boid.translation.distance(other_crows.translation);
-
 
         if diff != 0.0 && diff < VISION_RADIUS {
             let direction = other_crows.forward();
@@ -175,12 +178,11 @@ fn calculate_alignment(boid: &Transform, others: &Query<&Transform, With<Crow>>)
     total_alignment.normalize_or_zero()
 }
 
-fn calculate_cohesion(boid: &Transform, others: &Query<&Transform, With<Crow>>) -> Vec3{
+fn calculate_cohesion(boid: &Transform, others: &Query<&Transform, With<Crow>>) -> Vec3 {
     let mut average_position: Vec3 = Vec3::ZERO;
     let mut count: u16 = 0;
     for other_crows in others.iter() {
         let diff: f32 = boid.translation.distance(other_crows.translation);
-
 
         if diff != 0.0 && diff < VISION_RADIUS {
             count += 1;
@@ -191,5 +193,3 @@ fn calculate_cohesion(boid: &Transform, others: &Query<&Transform, With<Crow>>) 
 
     (average_position - boid.translation).normalize_or_zero()
 }
-
-
