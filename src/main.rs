5e4854072d759ee24e05c6d46d7c44d24cd9ede3
unknown <vladnohai@gmail.com>
2023-12-19 21:08:10 +0100

    crow model and animation are now displayed, animation is glitchy

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() {
         .add_plugins(PanOrbitCameraPlugin)
         .add_systems(Update, bevy::window::close_on_esc)
         .add_systems(Startup, setup)
-        
+        .add_systems(Update, run_animation)
         .add_systems(Update, system)
         .add_systems(Update, apply_velocity)
         .add_systems(Update, crow_behaviour)
@@ -48,16 +48,26 @@ impl Default for Crow {
 
 #[derive(Bundle)]
 struct CrowBundle {
-    pbr: PbrBundle,
+    pbr: SceneBundle,
     crow: Crow,
 }
 
 impl Default for CrowBundle {
     fn default() -> Self {
         Self {
-            pbr: PbrBundle::default(),
+            pbr: SceneBundle::default(),
             crow: Crow::default(),
         }
+    }
+}
+
+#[derive(Resource)]
+pub struct Animations(Vec<Handle<AnimationClip>>);
+
+
+pub fn run_animation(animations : Res<Animations>, mut players_query : Query<&mut AnimationPlayer, Added<AnimationPlayer>>){
+    for mut player in &mut players_query{
+        player.play(animations.0[0].clone_weak()).repeat();
     }
 }
 
@@ -65,6 +75,7 @@ fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>
 ) {
 
     // Flying Camera
@@ -123,7 +134,7 @@ fn setup(
         ..default()
     });
 
-
+    commands.insert_resource(Animations(vec![asset_server.load("crow1.glb#Animation0")]));
 
     //paddle
     let size: usize = 1000;
@@ -135,15 +146,15 @@ fn setup(
         let y_coords = rng.gen_range(-10000..10000) as f32 / 1000.0;
         let z_coords = rng.gen_range(-10000..10000) as f32 / 1000.0;
         let cube = CrowBundle {
-            pbr: PbrBundle {
-                mesh: meshes.add(Mesh::from(shape::Cube { size: 0.1 })),
-                material: materials.add(Color::rgb_u8(124, 144, 166).into()),
-                transform: Transform::from_xyz(x_coords, y_coords, z_coords),
-                ..default()
-            },
+            pbr : SceneBundle {
+            scene: asset_server.load("crow1.glb#Scene0"),
+            transform: Transform::from_xyz(x_coords, y_coords, z_coords)
+            .with_scale(Vec3::splat(0.02)),
             ..default()
+            },
+        ..default()
         };
-        all_cubes.push(cube)
+        all_cubes.push(cube);
     }
     commands.spawn_batch(all_cubes);
 }
