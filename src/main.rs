baf7ff2c732a6790f65d1888c3c1cc26132f5c17
TjerkWeijens <74548425+TjerkWeijens@users.noreply.github.com>
2023-12-20 16:07:10 +0100

    Grid has entity & actual proportional coordinate system

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
-use bevy::{math::*, prelude::*};
+use bevy::ecs::query;
+use bevy::{math::*, prelude::*, transform};
 use nalgebra::ComplexField;
 use rand::{thread_rng, Rng};
 
@@ -25,15 +26,16 @@ fn main() {
 struct Grid {
     grid: Vec<Vec<Vec<GridCell>>>,
     size: usize,
+    cell_size: f32,
 }
 
 struct GridCell {
-    crows: Vec<(Transform,Crow)>,
+    crows: Vec<Entity>,
 }
 
 impl Grid {
     //Create new grid with size*size*size, size must be even
-    fn New (size: usize) -> Self {
+    fn new (size: usize, cell_size: f32) -> Self {
         let mut grid = Vec::with_capacity(size);
         for x in 0..size {
             let mut grid_x = Vec::with_capacity(size);
@@ -49,21 +51,54 @@ impl Grid {
         Self {
             grid,
             size,
+            cell_size,
         }
     }
     //Add a crow to the grid by its transform centered around (0,0,0)
-    fn Add (&mut self, transform: Transform, crow: Crow) {
+    fn add (&mut self, query: Query<&Transform>, entity_id : Entity) {
         //Convert the possible negative coordinates to positive 
         //meaning that negative coordinates are between 0 and size/2 
         //and positive coordinates are between size/2 and size 
-        let x = (transform.translation.x.abs() + if transform.translation.x < 0.0 {0.0} else {self.size as f32 / 2.0}) as usize / self.size ;
-        let y = (transform.translation.y.abs() + if transform.translation.y < 0.0 {0.0} else {self.size as f32 / 2.0}) as usize / self.size ;
-        let z = (transform.translation.z.abs() + if transform.translation.z < 0.0 {0.0} else {self.size as f32 / 2.0}) as usize / self.size ;
-        self.grid[x][y][z].crows.push((transform, crow));
+        let transform = query.get(entity_id).unwrap();
+        let x = self.cooridnate_to_grid_coordinate(transform.translation.x);
+        let y = self.cooridnate_to_grid_coordinate(transform.translation.y);
+        let z = self.cooridnate_to_grid_coordinate(transform.translation.z);
+        self.grid[x][y][z].crows.push(entity_id);
     }
+
+    fn cooridnate_to_grid_coordinate (&self, coordinate: f32) -> usize {
+        ((coordinate.abs() + if coordinate < 0.0 {0.0} else {self.size as f32 * self.cell_size / 2.0}) / self.cell_size) as usize
+    }
+
+    //Get all crows in a certain radius around a certain point
+    fn get (&self, query: Query<&Transform>, point: Vec3, radius: f32) -> Vec<Entity> {
+        let mut crows = Vec::new();
+        //Get grid coordinates of the potential affected cells
+        let min_x = self.cooridnate_to_grid_coordinate(point.x - radius).max(0);
+        let max_x = self.cooridnate_to_grid_coordinate(point.x + radius).min(self.size);
+        let min_y = self.cooridnate_to_grid_coordinate(point.y - radius).max(0);
+        let max_y = self.cooridnate_to_grid_coordinate(point.y + radius).min(self.size);
+        let min_z = self.cooridnate_to_grid_coordinate(point.z - radius).max(0);
+        let max_z = self.cooridnate_to_grid_coordinate(point.z + radius).min(self.size);
+        //Iterate over all cells in the area grid
+        for x in min_x..max_x {
+            for y in min_y..max_y {
+                for z in min_z..max_z {
+                    //Iterate over all crows in the cell
+                    for crow in &self.grid[x][y][z].crows {
+                        //Check if the crow is in the radius
+                        if query.get(*crow).unwrap().translation.distance(point) < radius {
+                            crows.push(*crow);
+                        }
+                    }
+                }
+            }
+        }
+        crows
+    }
+
     //Update the grid by reevaluating the position of all crows
-    fn Update (&mut self) {
-        
+    fn update (&mut self) {
     }
 }
 
